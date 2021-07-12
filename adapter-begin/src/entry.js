bbfa14d4ce8d1cf1f1694effb327960b44043fd7
Ryan Bethel <ryan.bethel@gmail.com>
2021-07-12 10:43:28 -0400

    static.json workaround

--- adapter-begin/src/entry.js
+++ adapter-begin/src/entry.js
@@ -6,6 +6,7 @@ import '@sveltejs/kit/install-fetch'
 //import app from '../output/server/app.js'; // eslint-disable-line import/no-unresolved
 import { init, render } from '../output/server/app.js'; // eslint-disable-line import/no-unresolved
 import arc from '@architect/functions'
+
 init();
 //const url = require('url')
 //const app = require('@architect/shared/app.js')
