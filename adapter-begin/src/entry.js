5b86a5553b2b06137a1d67763971093665338a9d
Ryan Bethel <ryan.bethel@gmail.com>
2021-07-12 11:46:04 -0400

    hope

--- adapter-begin/src/entry.js
+++ adapter-begin/src/entry.js
@@ -22,7 +22,7 @@ export async function handler(event) {
 		let staticPath = arc.static(path)
 		return {
 			statusCode: 302,
-			Headers: `Location:"${staticPath}"`
+			headers: {'location':`"${staticPath}"`}
 		}
 	} catch (e) {
 		console.log('falling to server render')
