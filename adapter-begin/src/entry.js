833e4c57aa3172dcde41465ac335bb937aba2b76
Ryan Bethel <ryan.bethel@gmail.com>
2021-07-15 17:06:01 -0400

    test

--- adapter-begin/src/entry.js
+++ adapter-begin/src/entry.js
@@ -6,25 +6,32 @@ import '@sveltejs/kit/install-fetch'
 //import app from '../output/server/app.js'; // eslint-disable-line import/no-unresolved
 import { init, render } from '../output/server/app.js'; // eslint-disable-line import/no-unresolved
 import arc from '@architect/functions'
+import { proxy } from '../../proxy-test/proxy'
 
 init();
 //const url = require('url')
 //const app = require('@architect/shared/app.js')
 
 // TODO: run init() on the app before handling routes
+// 
 
-export async function handler(event) {
+async function checkStatic(req){ proxy(req, {passthru:true})}
+// async function render(req) { svelteRender(req)}
+exports.handler = arc.http.async(checkStatic, handler)
+
+// export async function handler(event) {
+async function handler(event) {
 	const { host, rawPath: path, httpMethod, rawQueryString, headers, body } = event;
 	
-	try {
-		console.log({path})
-		console.log(arc.static(path))
-		let staticPath = arc.static(path)
-		return {
-			statusCode: 302,
-			headers: {'location':staticPath}
-		}
-	} catch (e) {
+	// try {
+	// 	console.log({path})
+	// 	console.log(arc.static(path))
+	// 	let staticPath = arc.static(path)
+	// 	return {
+	// 		statusCode: 302,
+	// 		headers: {'location':staticPath}
+	// 	}
+	// } catch (e) {
 		console.log('falling to server render')
 		console.log({path})
 		
@@ -55,5 +62,5 @@ export async function handler(event) {
 	};
 }
 
-}
+// }
 
