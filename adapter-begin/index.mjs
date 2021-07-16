e65509924f88818588cbe865a3a6f17ceac344a4
Ryan Bethel <ryan.bethel@gmail.com>
2021-07-16 16:48:08 -0400

    t

--- adapter-begin/index.mjs
+++ adapter-begin/index.mjs
@@ -36,15 +36,15 @@ export default function () {
 			await esbuild.build({
 				entryPoints: ['.svelte-kit/begin/entry.js'],
 				// outfile: join('.begin', 'render', 'entry-index.js'),
-				outfile: join('.begin', 'render', 'index.js'),
+				outfile: join('.begin', 'render', 'entry-index.js'),
 				bundle: true,
 				platform: 'node'
 			})
 
-			// writeFileSync(join('.begin', 'render', 'index.js'), `
-			// 'use strict';
-			// const handler= require('./entry-index.js')
-		  // exports.handler = handler`)
+			writeFileSync(join('.begin', 'render', 'index.js'), `
+			'use strict';
+			const {handler}= require('./entry-index.js')
+		  module.exports= handler`)
 
 			utils.log.minor('Parsing app.arc file');
 			//const { static: static_mount_point } = parse_arc('app.arc');
