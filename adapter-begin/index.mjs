337b35e7dd3277a2e6f9f05bd5cfce0a839c922d
Ryan Bethel <ryan.bethel@gmail.com>
2021-07-16 16:23:32 -0400

    f

--- adapter-begin/index.mjs
+++ adapter-begin/index.mjs
@@ -42,7 +42,7 @@ export default function () {
 
 			writeFileSync(join('.begin', 'render', 'index.js'), `
 			'use strict';
-			const {handler}= require('./entry-index.js')
+			const handler= require('./entry-index.js')
 		  exports.handler = handler`)
 
 			utils.log.minor('Parsing app.arc file');
