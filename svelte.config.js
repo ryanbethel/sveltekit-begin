ab61f97572906d7d3e3ad145e12bf9330acabaf1
Ryan Bethel <ryan.bethel@gmail.com>
2021-07-18 17:59:42 -0400

    d

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 // import netlify from '@sveltejs/adapter-netlify';
 // import begin from './adapter-begin/index.mjs';
-import begin from '@ryanbethel/sveltekit-begin-adapter/index.mjs';
+import begin from '@ryanbethel/sveltekit-begin-adapter';
 
 export default {
 	kit: {
